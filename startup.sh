712c5fac43f4f8c631f4cbe7226187146b6207cf
Adil Elamin <mr-elamin@hotmail.com>
2024-12-06 21:27:06 +0000

    removed my ssh public key from the startup script

--- startup.sh
+++ startup.sh
@@ -19,9 +19,6 @@ echo "Setting up SSH authorized keys..."
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
-# Add the public key to the authorized_keys file
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHW6dBL66n0J0F9YY0YYgerXAcZblVdFKdAXS1BEnuG8nUtu/F97g9ca4vtjkkYuyUMyCtDdEjzLPqEtiGZXqXChETfDgFGQZnnlGVhI1NHyO9/zHu9s1ciaIHmb/HWe0mX+j2QGM+EEW7WSOk52mJ+qmS30JVPKZAbKuA+SjyGr0+s49lZLyjRVkqkU8YXZj3wDoqlYXvIjg8+LEAujGkof0HgxZRKPLC+2Gz0oFiTeqzcp5FRucUivj4Chn2MnjN7EULlXcxcvk2DtNsoZ3H9PILEF12yiLThysBkBQHpwNADjum85Jc9ZVJkK9tIKaaEIQ7qb1VAhKRV7UBN/feZ+tKLaCN19lRyE4m2VJn6gW0Y1QBenRTcEryw3LxI/H45srZ7XV7maCoDKEGESfGjbGsmPb2uGEn537K/trOw75kga+FTRYM+EgH3HnWbyW7cJm9/owja+zx7DN9WBTRHEpAeLwYO13XCtHMRDp0NRS455kNdE0uL/+wKUrvVCEg50GfcoigK38sZe11DTdkCgsfIKwVp6zYlqjFTnn2zAiEflPEZboNRm7ta3fNVyYO3RLGLS4n8vhWxHlTR75a/TxunUZsypvriLjKTQ0gyHH5FJJGIIdM8D/Smr/4Ovytd1nKHigz8yNaILViCpcFJ/mloa5ry/9U0pSocRxH8w== mr-elamin@pop-os" > /root/.ssh/authorized_keys
-
 # Set the correct permissions for the authorized_keys file
 chmod 600 /root/.ssh/authorized_keys
 chown -R root:root /root/.ssh
